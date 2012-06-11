85012fdabb2ac5b890e9058ee0c6bdc510a29973
Garrett Heath Koller <garrettheath4@gmail.com>
2012-06-11 13:31:43 -0400

    Script now ignores self-assigned IP addresses

--- hasNoIPaddr.sh
+++ hasNoIPaddr.sh
@@ -10,6 +10,11 @@ if [ -z "`ifconfig $WIFI | fgrep 'inet '`" ]; then
 	echo 'Wi-Fi: No IP address'
 	exit 0
 else
-	echo 'Wi-Fi: IP address'
-	exit 1
+	if [ -z "`ifconfig $WIFI | fgrep 'inet 169.254.'`" ]; then
+		echo 'Wi-Fi: IP address'
+		exit 1
+	else
+		echo 'Wi-Fi: No IP address (self-assigned)'
+		exit 0
+	fi
 fi
