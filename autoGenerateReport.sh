f98a03cad6da600a4c429b60c916aecf99768cf1
Garrett Heath Koller <garrettheath4@gmail.com>
2012-06-19 16:52:31 -0400

    Simplified log collection and added more logs to list

--- autoGenerateReport.sh
+++ autoGenerateReport.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 
 SYSINFO=~/System.spx
+SYSLOGDIR=/var/log
+
+# Logs to collect
+LOGS="/var/log/kernel.log
+/var/log/system.log
+/var/log/wifi.log
+/var/log/com.apple.IPConfiguration.bootp
+/var/log/com.apple.IPConfiguration.DHCPv6"
 
 DATE="`date +%Y.%m.%d-%H.%M.%S`"
 REPORTDIR=~/Desktop/"$DATE"
 
+# Create folder to store report data
 mkdir "$REPORTDIR"
 echo "Saving logs in $REPORTDIR"
 
@@ -21,7 +30,13 @@ fi
 sleep 60
 
 # Gather kernel logs
-tar cvzf "$REPORTDIR/kerneltar.tgz" /var/log/kernel.* /var/log/wifi.log
+for log in $LOGS; do
+	if [ -f "$log" ]; then
+		cp "$log" "$REPORTDIR/"
+	else
+		echo "Warning: '$log' does not exist"
+	fi
+done
 
 # Run AirPort scan
 /System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I -scan > "$REPORTDIR/AirportScan.txt"
