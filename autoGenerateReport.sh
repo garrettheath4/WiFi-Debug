0c1ad6514d1ea66f0a4fba1f04ed6f88f1e4fd07
Garrett Heath Koller <garrettheath4@gmail.com>
2012-07-05 17:05:46 -0400

    Added tcpdump process service functionality, but pidfile unnecessary

--- autoGenerateReport.sh
+++ autoGenerateReport.sh
@@ -8,7 +8,8 @@ LOGS="/var/log/kernel.log
 /var/log/system.log
 /var/log/wifi.log
 /var/log/com.apple.IPConfiguration.bootp
-/var/log/com.apple.IPConfiguration.DHCPv6"
+/var/log/com.apple.IPConfiguration.DHCPv6
+/Users/WiFi/Desktop/dhcptrace.pcap"
 
 DATE="`date +%Y.%m.%d-%H.%M.%S`"
 REPORTDIR=~/Desktop/"$DATE"
@@ -33,6 +34,46 @@ else
 	echo "Please save the system information report as $SYSINFO"
 fi
 
+# Make sure tcpdump service is running in the background collecting logs
+#  if not, start it in the background and create a pidfile
+TCPPIDFILE=~/Desktop/tcpdump.pid
+RUNTCPDUMP=1	# NO, don't run tcpdump (default)
+
+if [ -f "$TCPPIDFILE" ]; then
+	# Make sure tcpdump is still running for this pid
+	TCPPID="`cat $TCPPIDFILE | head -n1`"
+	ps -p "$TCPPID"
+	if [ "$?" -eq 0 ]; then
+		echo "tcpdump is running at the expected pid $TCPPID"
+		RUNTCPDUMP=1	# NO, don't run tcpdump
+	else
+		echo "tcpdump is not running; invalid pidfile: $TCPPID"
+		RUNTCPDUMP=0	# YES, run tcpdump
+		rm "$TCPPIDFILE"
+	fi
+else
+	echo "No pidfile. Starting tcpdump and creating the pidfile."
+	RUNTCPDUMP=0	# YES, run tcpdump
+fi
+
+if [ "$RUNTCPDUMP" -eq 0 ]; then	# if YES
+	# Can I run passwordless sudo?
+	SUDO="`which sudo` -n"
+	sudo -n echo hi
+	if [ "$?" -eq 1 ]; then
+		echo "Warning: Unable to use sudo to get a tcpdump"
+		SUDO=""
+	fi
+
+	# Start running tcpdump in the background
+	$SUDO tcpdump -i "$WIFI" -p -s0 -vv -w ~/Desktop/dhcptrace.pcap "port bootps" &
+	SUDOPID="$!"
+	sleep 1
+	TCPPID=`ps -Af | awk '$3 == '"$SUDOPID"' { print $2 }'`
+	echo "tcpdump started with pid $TCPPID"
+	echo "$TCPPID" > "$TCPPIDFILE"
+fi
+
 sleep 60
 
 # Gather kernel logs
@@ -47,11 +88,3 @@ done
 # Run AirPort scan
 /System/Library/PrivateFrameworks/Apple80211.framework/Resources/airport -I -scan > "$REPORTDIR/AirportScan.txt"
 
-# Collect TCP Dump
-sudo killall -SIGTERM tcpdump
-touch "$REPORTDIR/dhcptrace.pcap"
-sudo -n tcpdump -i "$WIFI" -p -s0 -vv -w "$REPORTDIR/dhcptrace.pcap" "port bootps" &
-if [ "$?" -eq 1 ]; then
-	echo "Warning: Unable to use sudo to get a tcpdump"
-	tcpdump -i "$WIFI" -p -s0 -vv -w "$REPORTDIR/dhcptrace.pcap" "port bootps"
-fi
